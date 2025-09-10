7cdee6496a13395fbb36a5117dd3f34af09e30fe
Ruben.Gabrielyan <gabrielianruben@gmail.com>
2025-09-10 10:42:25 +0400

    change

--- src/AudioGenerate.tsx
+++ src/AudioGenerate.tsx
@@ -7,24 +7,25 @@ export default function AudioGenerator() {
     const generateAudio = async () => {
         setLoading(true);
         try {
+            const formData = new FormData();
+            formData.append("project_id", "13333333212121");
+            formData.append("text", "Արա Ռուբ ջան ինչ կա ախպերս, ոնց ես");
+            formData.append("voice", "Areg");
+            formData.append("format", "mp3");
+
             const response = await fetch("https://wav.am/generate_audio/", {
                 method: "POST",
                 headers: {
                     "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiIxOTI1ZDU3ZDYwZDA0MzBmOTVlNWNlNmIwM2M1MGNhZSIsInVzZXJuYW1lIjoiUnViZW5HYWJyaWVsaWFuIiwiY29ubmVjdGlvbiI6ImFwaSIsImV4cCI6MTc2MTg2ODgwMCwiaWF0IjoxNzU3MDY5ODQxfQ.Ot8NeOf43M0bSkwVjYDL3E-D-ENogREhr_SZ9f-kbMs",
-                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify({
-                    project_id: "12345",
-                    text: "Արա Ռուբ ջան ինչ կա ախպերս, ոնց ես",
-                    voice: "Areg",
-                    format: "mp3",
-                }),
+                body: formData,
             });
 
+            console.log(response);
             // if API returns binary MP3
-            const blob = await response.blob();
-            const url = URL.createObjectURL(blob);
-            setAudioUrl(url);
+            // const blob = await response.blob();
+            // const url = URL.createObjectURL(blob);
+            // setAudioUrl(url);
         } catch (error) {
             console.error("Error generating audio:", error);
         } finally {
